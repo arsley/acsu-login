06203d1058f36863b79c43aa73cd87e1b20af35f
assly <blue20will@gmail.com>
2018-07-04 14:28:37 +0900

    Initial commit

---
+++ login.js
@@ -0,0 +1,34 @@
+const readlineSync = require('readline-sync'),
+      axios = require('axios'),
+      chalk = require('chalk');
+
+const uid = readlineSync.question('Education number > ');
+const pwd = readlineSync.question('Password > ', { hideEchoBack: true });
+
+/**
+ * @param {Promise object} response
+ * @return {Boolean} [Return status of login]
+ */
+function assertLogin(response) {
+  const status = response.data.match(/Login \w/)[0].split(' ');
+  return status[1] === 'S' ? true : false;
+}
+
+axios
+  .get('https://login.shinshu-u.ac.jp/cgi-bin/Login.cgi', {
+    params: {
+      uid: uid,
+      pwd: pwd
+    }
+  })
+  .then(function(response) {
+    console.log(
+      assertLogin(response)
+        ? chalk.green(`Login Success.`)
+        : chalk.red(`Login Failure.`)
+    );
+  })
+  .catch(function(error) {
+    console.error(chalk.red('Somethings wrong...'));
+    console.info(chalk.blue('Send issue to author!'));
+  });
---
+++ readme.md
@@ -0,0 +1,41 @@
+# ACSU Login
+
+ACSU authenticate from console with Node/JavaScript:tada:
+
+## Prerequisites
+
+- node
+- yarn (recommended) of npm
+
+## Setup
+
+```
+yarn
+# or
+npm i
+```
+
+## Usage
+
+Type
+
+```
+yarn acsulogin
+# or
+npm run acsulogin
+# or
+node login.js
+```
+
+then will show:
+
+```
+Education number > *your education number*
+Password > *your password (printed '*')*
+```
+
+## Dependencies
+
+- axios (Accsess to login page)
+- readlineSync (Intaractive interface)
+- chalk (CLI coloring)
